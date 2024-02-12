32a525eb27809a0334ce217c3fa7d1109938df07
Mariana Granados <mgranados09@alumnos.uaq.mx>
2024-02-12 16:29:35 -0600

    code for rgb for new pcb implemented

---
+++ ss_rgb.ino
@@ -0,0 +1,140 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server;
+    
+    uint8_t pin_alarm = 4;
+    uint8_t pin_motion = 1;
+    uint8_t pin_pres = 16;
+
+    uint8_t rPin = 14;
+    uint8_t gPin = 0;
+    uint8_t bPin = 2;
+
+    char* ssid = "PCUAQ82";
+    char* password = "PCUAQ8282";
+    String alarm_state;
+    String motion_state;
+    String pres_state;
+    String total_state;
+    
+    char webpage[] PROGMEM = R"=====(
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sensor states</title>
+    </head>
+    <body>
+    </body>
+    
+    </html>
+    <script>
+    function myFunction()
+    {
+      console.log("boton presionado");
+      var xhr = new XMLHttpRequest();
+      var url = "/ledstate";
+      xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("led-state").innerHTML = this.responseText;
+        }
+      };
+      xhr.open("GET", url, true);
+      xhr.send();
+    };
+    document.addEventListener('DOMContentLoaded', myFunction, false);
+    </script>
+    </html>
+    )=====";
+    
+    void setup()
+    {
+      pinMode(pin_alarm, INPUT);
+      pinMode(pin_motion, INPUT);
+      pinMode(pin_pres, INPUT);
+
+      pinMode(rPin, OUTPUT); 
+      pinMode(gPin, OUTPUT); 
+      pinMode(bPin, OUTPUT); 
+
+      WiFi.begin(ssid,password);
+
+      Serial.begin(115200);
+      while(WiFi.status()!=WL_CONNECTED)
+      { 
+        Serial.print(".");
+        analogWrite(rPin,   1);
+        analogWrite(gPin, 255);
+        analogWrite(bPin,  1);
+        delay(500);
+      }
+      Serial.println("");
+      Serial.print("IP Address: ");
+      Serial.println(WiFi.localIP());
+
+      analogWrite(rPin,   1);
+      analogWrite(gPin, 1);
+      analogWrite(bPin,  255);
+    
+      server.on("/",[](){server.send_P(200,"text/html", webpage);});
+      server.on("/state",getState);
+      server.begin();
+    }
+    
+    void loop()
+    {
+      if(WiFi.status()!=WL_CONNECTED)
+      {
+          analogWrite(rPin,   1);
+          analogWrite(gPin, 255);
+          analogWrite(bPin,  1);
+          //Serial.print(".");
+      }
+      else
+      {
+        analogWrite(rPin,   1);
+        analogWrite(gPin, 1);
+        analogWrite(bPin,  255);
+        //Serial.println("");
+        //Serial.print("IP Address: ");
+        //Serial.println(WiFi.localIP());
+        server.handleClient();
+        getState();
+      }
+      
+    }
+    
+    
+    void getState()
+    {
+
+      int pinState_A = digitalRead(pin_alarm);
+      int pinState_P = digitalRead(pin_pres);
+      
+      /*
+      alarm_state = digitalRead(pin_alarm) ? "{\n \t\"gas\": 1 \n" : "{\n \t\"gas\": 0, \n";
+      motion_state = digitalRead(pin_motion) ? "\n \t\"motion\": 1 \n" : "\n \t\"motion\": 0, \n";
+      pres_state = digitalRead(pin_pres) ? "\n \t\"presence\": 1 \n}" : "\n \t\"presence\": 0 \n}";
+      */
+     // alarm_state = "{\n \t gas: 1, \n";
+     // motion_state = digitalRead(pin_motion) ? "\n \t\"motion\": 1 \n" : "\n \t\"motion\": 0, \n";
+     if (pinState_A == HIGH) {
+        alarm_state = "{\n \t alarm: 1, \n";
+      } else {
+        alarm_state = "{\n \t alarm: 0, \n";
+      }
+      if (pinState_P == HIGH) {
+        pres_state = "\n\t window: 1 \n}";
+      } else {
+        pres_state = "\n\t window: 0 \n}";
+      }
+      motion_state = "\n\t pir: 0, \n";
+      //pres_state = "\n \t presence: 1 \n}";-
+
+      total_state = alarm_state+motion_state+pres_state;
+
+      server.send(200,"text/plain", total_state);
+    }
